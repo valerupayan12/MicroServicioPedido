ee874e21f951b3c730fb86805b2805af86d113df
valerupayan12 <va.rupayan@duocuc.cl>
2026-05-19 13:32:16 -0400

    Add files via upload

---
+++ MicroPedido/src/main/java/com/example/MicroPedido/MicroPedidoApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroPedido;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroPedidoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroPedidoApplication.class, args);
+	}
+
+}
---
+++ MicroPedido/src/test/java/com/example/MicroPedido/MicroPedidoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroPedido;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroPedidoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
